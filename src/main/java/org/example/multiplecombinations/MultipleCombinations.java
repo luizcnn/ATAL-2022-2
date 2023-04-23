b06a478b69b589643590602762ee8a5f125dc959
Luiz Bonfim <luiz.bonfim.neto@ccc.ufcg.edu.br>
2023-04-23 18:29:15 -0300

    removing ununsed classes

--- src/main/java/org/example/multiplecombinations/MultipleCombinations.java
+++
@@ -1,9 +0,0 @@
-package org.example.multiplecombinations;
-
-import java.util.List;
-
-public interface MultipleCombinations {
-
-  void printMultipleSet(List<Integer> values, Integer targetValue);
-
-}
